1b700bb656cc40e2897bcdc4e38ed1eb2c52e042
jayxdcode <alt444.001@gmail.com>
2025-05-10 00:42:22 +0800

    update

--- start.sh
+++ start.sh
@@ -2,12 +2,16 @@
 
 # Step 1: Install ffmpeg
 echo "[*] Installing ffmpeg..."
-sudo apt update && sudo apt install -y ffmpeg
+sudo apt update && sudo apt install -y ffmpeg python3
 
 # Step 2: Locate the ffmpeg binary
 echo "[*] Locating ffmpeg binary..."
 FFMPEG_PATH=$(which ffmpeg)
 echo "Located ffmpeg at: $FFMPEG_PATH"
+
+#Step 3: Install requirements
+echo "[*] Installing requirements..."
+sudo pip install -r requirements.txt
 
 # Run
 python3 main.py
